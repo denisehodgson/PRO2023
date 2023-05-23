4bbe4b7903b288815dcffb4f1f410d79233c7ad7
denisehodgson <58290703+denisehodgson@users.noreply.github.com>
2023-05-23 17:08:09 +0200

    Update sketch.js

--- Challenge 2/sketch.js
+++ Challenge 2/sketch.js
@@ -1,4 +1,3 @@
-
 let backgroundImage, sand, snow;
 let clockImage, water, ice;
 let secondImage, swim, skate;
@@ -6,43 +5,44 @@ let minuteImage, ball, puck;
 let hourImage, guy, girl;
 let mouseImage, sun, snowflake;
 
-function preload(){
+let positions = [];
+
+function preload() {
   //backgroundImage
-  sand = loadImage("images/sand.png")
-  snow = loadImage("images/snow.jpeg")
+  sand = loadImage("images/sand.png");
+  snow = loadImage("images/snow.jpeg");
 
   //clockImage
-  water = loadImage("images/water.png")
-  ice = loadImage("images/ice.png")
+  water = loadImage("images/water.png");
+  ice = loadImage("images/ice.png");
 
- //secondImage
-  swim = loadImage("images/swim.png")
-  skate = loadImage("images/skate.png")
+  //secondImage
+  swim = loadImage("images/swim.png");
+  skate = loadImage("images/skate.png");
 
   //minuteImage
-  ball = loadImage("images/ball.png")
-  puck = loadImage("images/puck.png")
-
+  ball = loadImage("images/ball.png");
+  puck = loadImage("images/puck.png");
 
   //hourImage
-  guy = loadImage("images/guy.png")
-  girl = loadImage("images/girl.png")
+  guy = loadImage("images/guy.png");
+  girl = loadImage("images/girl.png");
 
   //mouseImage
-  sun = loadImage("images/sun.png")
-  snowflake = loadImage("images/snowflake.png")
+  sun = loadImage("images/sun.png");
+  snowflake = loadImage("images/snowflake.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  
-    backgroundImage = sand;
-    clockImage = water;
-    secondImage = swim;
-    minuteImage = ball;
-    hourImage = guy;
-    mouseImage = sun;
+
+  backgroundImage = sand;
+  clockImage = water;
+  secondImage = swim;
+  minuteImage = ball;
+  hourImage = guy;
+  mouseImage = sun;
 }
 
 function draw() {
@@ -50,17 +50,27 @@ function draw() {
   background(backgroundImage);
 
   //Mouse
-  image(mouseImage,mouseX-100,mouseY-100,200,200);
+  image(mouseImage, mouseX - 75, mouseY - 75, 150, 150);
 
-  translate(windowWidth/2, windowHeight/2);
+  if (mouseIsPressed === true) {
+    for (let i = 0; i < positions.length; i++) {
+      image(mouseImage, positions[i][0], positions[i][1], 30, 30);
+      positions[i][1]++;
+    }
+    positions.push([random(width), random(height)]);
+  } else {
+    positions = [];
+  }
 
-  let hr = nf(hour(), 2, 0); 
-  let mn = nf(minute(), 2, 0); 
+  translate(windowWidth / 2, windowHeight / 2);
+
+  let hr = nf(hour(), 2, 0);
+  let mn = nf(minute(), 2, 0);
   let sc = nf(second(), 2, 0);
   let mos = month();
 
   //Clock
-  image(clockImage,-250,-250,500,500);
+  image(clockImage, -250, -250, 500, 500);
 
   rotate(-90);
 
@@ -68,25 +78,25 @@ function draw() {
   let scAngle = map(sc, 0, 60, 0, 360);
   push();
   rotate(scAngle);
-  image(secondImage,80,0,170,170);
+  image(secondImage, 80, 0, 170, 170);
   pop();
 
   //Minute
   let mnAngle = map(mn, 0, 60, 0, 360);
   push();
   rotate(mnAngle);
-  image(minuteImage,110,0,50,50);
+  image(minuteImage, 110, 0, 50, 50);
   pop();
 
   //Hour
   let hrAngle = map(hr % 12, 0, 12, 0, 360);
   push();
   rotate(hrAngle);
-  image(hourImage,-10,0,110,110);
-  pop(); 
+  image(hourImage, -10, 0, 110, 110);
+  pop();
 
   //Between March and October the clock will turn back to a summer theme
-  if(mos > 3 && mos < 10){
+  if (mos > 3 && mos < 10) {
     backgroundImage = sand;
     clockImage = water;
     secondImage = swim;
@@ -95,18 +105,12 @@ function draw() {
     mouseImage = sun;
   }
   //If it's any of the other months, the clock will turn into a winter theme
-  else{
-  backgroundImage = snow;
-  clockImage = ice;
-  secondImage = skate;
-  minuteImage = puck;
-  hourImage = girl;
-  mouseImage = snowflake;
+  else {
+    backgroundImage = snow;
+    clockImage = ice;
+    secondImage = skate;
+    minuteImage = puck;
+    hourImage = girl;
+    mouseImage = snowflake;
   }
-}
-
-function mousePressed(){
-  image(mouseImage,mouseX-100,mouseY-100,200,200);
-}
-function mouseReleased(){
 }
